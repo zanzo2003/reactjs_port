b4b74a44ddd72755ebff497a5974e39d38b42f2d
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2023-11-08 20:14:33 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import profileImage from './photo.jpg';
+
+var profileImage = "https://e7.pngegg.com/pngimages/870/211/png-clipart-iphone-world-emoji-day-man-iphone-electronics-face-thumbnail.png";
 
 function App() {
   return (
     <div className="portfolio">
       <img src={profileImage} alt="Profile" className="profile-image" />
-      <h1>Saxam Rawat</h1>
-      <p>City: Dehradun, India</p>
-      <p>College Reg No: 21BCG10087</p>
-      <p>Email: saxamrawat@gmail.com</p>
-      <p>Mobile: +91 7060472549</p>
+      <h1>Shashwath Bhaskar</h1>
+      <p>City: Thiruvanamthapuram, India</p>
+      <p>College Reg No: 21BCE10646</p>
+      <p>Email: bhaskarshashwath@gmail.com</p>
+      <p>Mobile: +91 9567098994</p>
       <div className="social-links">
-        <a href="https://github.com/saxamrawat" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://www.linkedin.com/in/saxamrawat" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://www.instagram.com/saxamrawat" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="https://docs.google.com/document/d/1jmxWQjpuW7tiYJoANiA7p3njpgv1vZfn/edit?usp=sharing&ouid=112017365365283991142&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Resume</a>
+        <a href="https://github.com/zanzo2003" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/shashwath-bhaskar-a56597213/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://www.instagram.com/stop.itttt.shashhh/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://drive.google.com/file/d/1PeCN_ROEeRmxCtjPb06-Qhug3tcNS4zV/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
       </div>
     </div>
   );
